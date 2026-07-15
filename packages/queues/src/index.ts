919d7dc26c56fde06fd5faeab761c5b4cf9ddacc
Hritik Kaushik <hkk10699@gmail.com>
2026-07-16 00:32:44 +0530

    feat(queues): add slack-ingest and slack-sync BullMQ queues

--- packages/queues/src/index.ts
+++ packages/queues/src/index.ts
@@ -2,5 +2,7 @@ export { DOCUMENT_INGEST_QUEUE, createDocumentIngestQueue } from './document-ing
 export type { DocumentIngestJobPayload } from './document-ingest.queue.js';
 export { REPO_INGEST_QUEUE, REPO_SYNC_QUEUE, createRepoIngestQueue, createRepoSyncQueue } from './repo-queues.js';
 export type { RepoIngestJobPayload, RepoSyncJobPayload } from './repo-queues.js';
+export { SLACK_INGEST_QUEUE, SLACK_SYNC_QUEUE, createSlackIngestQueue, createSlackSyncQueue } from './slack-queues.js';
+export type { SlackIngestJobPayload, SlackSyncJobPayload } from './slack-queues.js';
 export { DEFAULT_JOB_OPTS } from './job-options.js';
 export type { ConnectionOptions } from 'bullmq';
